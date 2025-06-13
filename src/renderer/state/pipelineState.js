49edf27b2cf1d93d4294bdc8c253141692bb7af5
PoTrd <paul.taurand@gmail.com>
2025-06-13 13:17:50 +0200

    feat # clean pipeline state code

--- src/renderer/state/pipelineState.js
+++ src/renderer/state/pipelineState.js
@@ -1,54 +1,15 @@
-// src/renderer/state/pipelineState.js
-
 export class PipelineState {
     #pipelines = [];
-    #subscribers = [];
 
-    get pipelines() {
+    getPipelines() {
         return this.#pipelines;
     }
 
-    subscribe(fn) {
-        this.#subscribers.push(fn);
-    }
-
-    setPipelines(newPipelines) {
-        this.#pipelines = newPipelines;
-        this.#notify();
-    }
-
-    addPipelines(newOnes) {
-        const unique = newOnes.filter(newPipe =>
-        !this.#pipelines.some(p => p.id === newPipe.id)
-        );
-
-    this.#pipelines = [...this.#pipelines, ...unique].sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
-
-    this.#notify();
+    setPipelines(pipelinesArray) {
+        this.#pipelines = pipelinesArray;
     }
 
     removePipeline(id) {
         this.#pipelines = this.#pipelines.filter(p => p.id !== id);
-        this.#notify();
-    }
-
-    #notify() {
-        this.#subscribers.forEach(fn => fn(this.#pipelines));
     }
 }
-
-// class pipelinesState {
-//     constructor() {
-//         this.pipelines = [];
-//     }
-
-//     setPipelines(newPipelines) {
-//         this.pipelines = newPipelines;
-//     }
-
-//     removePipeline(id) {
-//         this.pipelines = this.pipelines.filter(p => p.id !== id);
-//     }
-// }
-
-// module.exports = { pipelinesState };
