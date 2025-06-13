717b8a109e2def489b5916bc9dafdea91dd31e2b
PoTrd <paul.taurand@gmail.com>
2025-06-13 13:53:40 +0200

    feat # Add updated status of finished pipelines

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,9 +1,7 @@
-import { fetchRunningPipelines } from './services/pipeline.service.js';
-import { PipelineState } from './state/pipelineState.js';
+import { fetchAndStorePipelines, getStoredPipelines, removePipeline } from './services/pipeline.service.js';
 import { Logger } from './services/logging.service.js';
 
 const container = document.getElementById('container');
-const state = new PipelineState();
 
 function renderPipelines(pipelines, container) {
     if (!container) return;
@@ -22,46 +20,15 @@ function renderPipelines(pipelines, container) {
     container.querySelectorAll('.delete-btn').forEach(btn => {
         btn.addEventListener('click', e => {
             const id = btn.getAttribute('data-id');
-            state.removePipeline(Number(id));
-            renderPipelines(state.getPipelines(), container);
+            const updated = removePipeline(Number(id));
+            renderPipelines(updated, container);
         });
     });
 }
 
-function mergePipeline(existingLst, incomingLst) {
-    const existingIds = new Set(existingLst.map(p => p.id));
-    const incomingIds = new Set(incomingLst.map(p => p.id));
-    const missingPipelines = existingLst.filter(p => !incomingIds.has(p.id));
-    const merged = [...existingLst];
-
-    incomingLst.forEach(p => {
-        if (existingIds.has(p.id)) {
-            const index = merged.findIndex(existing => existing.id === p.id);
-            if (index !== -1) {
-                merged[index] = p;
-                return;
-            }
-        }
-        merged.push(p);
-    });
-
-    const missingPipelinesIds = new Set(missingPipelines.map(p => p.id));
-    merged.forEach(p => {
-        if(missingPipelinesIds.has(p.id)) {
-            p.status = 'missing';
-        }
-    });
-
-    return merged;
-}
-
 async function main() {
     try {
-        const data = await fetchRunningPipelines();
-        Logger.info('Pipelines fetched:', data.length);
-        const existing = state.getPipelines();
-        const merged = mergePipeline(existing, data);
-        state.setPipelines(merged);
+        const merged = await fetchAndStorePipelines();
         renderPipelines(merged, container);
     } catch (e) {
         Logger.error('Fetch error:', e.message);
--- src/renderer/services/pipeline.service.js
+++ src/renderer/services/pipeline.service.js
@@ -1,16 +1,76 @@
-export async function fetchRunningPipelines() {
-    if (window.env && window.env.USE_MOCK === 'true') {
-        const data = await fetch('./services/mock-pipelines.json').then(r => r.json());
-        return data;
-    }
+import { Logger } from './logging.service.js';
+import { PipelineState } from '../state/pipelineState.js';
 
-    const res = await fetch(`${window.env.GITLAB_URL}/api/v4/projects/${window.env.GITLAB_PROJECT_ID}/pipelines?status=running`, {
-        headers: { 'PRIVATE-TOKEN': window.env.GITLAB_TOKEN },
+const state = new PipelineState();
+
+function mergePipeline(existingLst, incomingLst) {
+    const existingIds = new Set(existingLst.map(p => p.id));
+    const incomingIds = new Set(incomingLst.map(p => p.id));
+    const missingPipelines = existingLst.filter(p => !incomingIds.has(p.id));
+    const merged = [...existingLst];
+
+    incomingLst.forEach(p => {
+        if (existingIds.has(p.id)) {
+            const index = merged.findIndex(existing => existing.id === p.id);
+            if (index !== -1) {
+                merged[index] = p;
+                return;
+            }
+        }
+        merged.push(p);
     });
 
+    const missingPipelinesIds = new Set(missingPipelines.map(p => p.id));
+    merged.forEach(p => {
+        if(missingPipelinesIds.has(p.id)) {
+            const updatedPipeline = fetchPipeline(p.id);
+            if (updatedPipeline) {
+                const index = merged.findIndex(existing => existing.id === p.id);
+                if (index !== -1) {
+                    merged[index] = updatedPipeline;
+                }
+            }
+        }
+    });
+
+    return merged;
+}
+
+export async function fetchAndStorePipelines() {
+    let data;
+    if (window.env && window.env.USE_MOCK === 'true') {
+        data = await fetch('./services/mock-pipelines.json').then(r => r.json());
+    } else {
+        const res = await fetch(`${window.env.GITLAB_URL}/api/v4/projects/${window.env.GITLAB_PROJECT_ID}/pipelines?status=running`, {
+            headers: { 'PRIVATE-TOKEN': window.env.GITLAB_TOKEN },
+        });
+        if (!res.ok) {
+            throw new Error(`Erreur API GitLab: ${res.status}`);
+        }
+        data = await res.json();
+    }
+    Logger.info('Pipelines fetched:', data.length);
+    const existing = state.getPipelines();
+    const merged = mergePipeline(existing, data);
+    state.setPipelines(merged);
+    return merged;
+}
+
+export async function fetchPipeline(id) {
+    const res = await fetch(`${window.env.GITLAB_URL}/api/v4/projects/${window.env.GITLAB_PROJECT_ID}/pipelines/${id}`, {
+        headers: { 'PRIVATE-TOKEN': window.env.GITLAB_TOKEN },
+    });
     if (!res.ok) {
         throw new Error(`Erreur API GitLab: ${res.status}`);
     }
+    return await res.json();
+}
 
-    return res.json();
+export function getStoredPipelines() {
+    return state.getPipelines();
+}
+
+export function removePipeline(id) {
+    state.removePipeline(id);
+    return state.getPipelines();
 }
